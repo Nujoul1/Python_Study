f146cfa199a5f85a452d00e1ec8e2067c86c984f
Nujoul <1195475516@qq.com>
2026-01-12 01:07:54 +0800

    添加topics和topic/id页面

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -8,4 +8,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     # 主页
     path('', views.index, name='index'),
+    # 显示所有Topic的页面
+    path('topics/', views.topics, name='topics'),
+    # 显示特定Topic的页面
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
 ]
