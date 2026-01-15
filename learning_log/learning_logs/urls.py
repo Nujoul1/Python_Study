ad42ac0031c5a5193552035da13fddebc582c007
Nujoul <1195475516@qq.com>
2026-01-15 22:13:43 +0800

    添加新建topic, entry, edit entry页面

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -12,4 +12,10 @@ urlpatterns = [
     path('topics/', views.topics, name='topics'),
     # 显示特定Topic的页面
     path('topics/<int:topic_id>/', views.topic, name='topic'),
+    # 用于添加新Topic的页面
+    path('new_topic/', views.new_topic, name='new_topic'),
+    # 用于添加新的Entry的页面
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+    # 用于编辑已有Entry的页面
+    path('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'),
 ]
